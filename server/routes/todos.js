401d4ded209b88bc8c1d11dd6befab330fc12c6a
DWalkerIT <DWalkerIT@gmail.com>
2021-04-05 14:49:32 -0400

    backend todos update insomnia not working

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -33,13 +33,20 @@ router.post('/todos/:authorId/:description', async (req, res) => {
     res.json({message: 'todo created'})
 })
 
-router.patch('/todos/:authorId/:description', async (req, res) => {
+router.patch('/todos/todoId', async (req, res) => {
     // the description
-    const { description } = req.body
-    await conn.raw(`
-    INSERT INTO todos (description, status, user_id)
-    VALUES (?,?,?)
-    `, ['testing', 'active', 1])
-    res.json({message: 'todo created'})
+    const todoId = req.params.todoId
+    const { description, status } = req.body
+    // console.log(description,status)
+    // undefined active
+    await conn('todos')
+    .where({id: todoId})
+    .update(req.body)
+    // await conn.raw(`
+    // UPDATE todos
+    // SET description = ?, status = ?
+    // WHERE id = ?
+    // `, [description, status, todoId])
+    // res.json({message: 'todo updated'})
 })
 export default router
