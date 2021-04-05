17cfd673da5f2160eba9b34f5cc7d8e5861611c8
DWalkerIT <DWalkerIT@gmail.com>
2021-04-05 14:21:54 -0400

    crashed

--- server/routes/todos.js
+++ server/routes/todos.js
@@ -12,5 +12,34 @@ router.get('/todos', async (req, res) => {
     const rows = todos.rows
     res.json(rows)
 })
+//req.params
+router.delete('/todos/:id', async (req, res) => {
+    const todoId = req.params.Id
+    const todos = await conn.raw(`
+        DELETE FROM todos
+        WHERE user_id = ?;
+    `, [todoId])
+    // const rows = todos.rows
+    res.json({message: 'todo deleted'})
+})
 
+router.post('/todos/:authorId/:description', async (req, res) => {
+    // the description
+    const { description } = req.body
+    await conn.raw(`
+    INSERT INTO todos (description, status, user_id)
+    VALUES (?,?,?)
+    `, ['testing', 'active', 1])
+    res.json({message: 'todo created'})
+})
+
+router.patch('/todos/:authorId/:description', async (req, res) => {
+    // the description
+    const { description } = req.body
+    await conn.raw(`
+    INSERT INTO todos (description, status, user_id)
+    VALUES (?,?,?)
+    `, ['testing', 'active', 1])
+    res.json({message: 'todo created'})
+})
 export default router
